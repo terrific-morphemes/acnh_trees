ad9d1e46cc86c7c52ced05b1c7e2c96b7c914c4e
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2020-03-29 20:32:12 -0400

    use bootstrap forms

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'trees',
     'corsheaders',
     'rest_framework',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
